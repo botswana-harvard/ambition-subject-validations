3aebad26aab5aee33fbdafa7b1f0285d5f46a01d
Leslie <leslie.kep@gmail.com>
2017-06-06 17:29:09 +0200

    add radiology tests, all passing

--- ambition_subject_validations/validations/radiology.py
+++ ambition_subject_validations/validations/radiology.py
@@ -12,59 +12,72 @@ class Radiology(RequiredFieldValidationMixin):
         self.required_if(
             YES,
             field='is_cxr_done',
-            field_required='cxr_type')
+            field_required='cxr_type',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             YES,
             field='is_cxr_done',
-            field_required='when_cxr_done')
+            field_required='cxr_date',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
-            INFILTRATE_LOCATION,
+            'infiltrate_location',
             field='cxr_type',
-            field_required='infiltrate_location')
+            field_required='infiltrate_location',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             YES,
             field='is_cxr_done',
-            field_required='cxr_description')
+            field_required='cxr_description',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             YES,
             field='is_ct_performed',
-            field_required='is_scanned_with_contrast')
+            field_required='is_scanned_with_contrast',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             YES,
             field='is_ct_performed',
-            field_required='date_ct_performed')
+            field_required='date_ct_performed',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             YES,
             field='is_ct_performed',
-            field_required='brain_imaging_reason')
+            field_required='brain_imaging_reason',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             OTHER,
             field='brain_imaging_reason',
-            field_required='brain_imaging_reason_other')
+            field_required='brain_imaging_reason_other',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             YES,
             field='is_cxr_done',
-            field_required='are_results_abnormal')
+            field_required='are_results_abnormal',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             YES,
             field='are_results_abnormal',
-            field_required='abnormal_results_reason')
+            field_required='abnormal_results_reason',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             OTHER,
             field='abnormal_results_reason',
-            field_required='abnormal_results_reason_other')
+            field_required='abnormal_results_reason_other',
+            cleaned_data=self.cleaned_data)
 
         self.required_if(
             'infarcts',
             field='abnormal_results_reason',
-            field_required='if_infarcts_location')
+            field_required='if_infarcts_location',
+            cleaned_data=self.cleaned_data)
+        return self.cleaned_data
