fff664ee3a9963a70f7a2a0c04fc41919c9c1df5
Gofaone Magodi <magodign@magodign.local>
2017-06-06 14:49:00 +0200

    microbio tests

--- ambition_subject_validations/tests/test_microbiology.py
+++ ambition_subject_validations/tests/test_microbiology.py
@@ -1,12 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from edc_constants.constants import YES
+from edc_constants.constants import YES, NO
 
 from ..validations import Microbiology
 
 
 class TestMicrobiologyValidations(TestCase):
+
     def test_urine_culture_performed_require_urine_culture_results(self):
         cleaned_data = {'urine_culture_performed': YES,
                         'urine_culture_results': None}
@@ -17,3 +18,13 @@ class TestMicrobiologyValidations(TestCase):
                         'urine_culture_results': 'no_growth'}
         microbilogy = Microbiology(cleaned_data=cleaned_data)
         self.assertTrue(microbilogy.clean())
+
+        cleaned_data = {'urine_culture_performed': NO,
+                        'urine_culture_results': None}
+        microbilogy = Microbiology(cleaned_data=cleaned_data)
+        self.assertTrue(microbilogy.clean())
+
+        cleaned_data = {'urine_culture_performed': NO,
+                        'urine_culture_results': 'no_growth'}
+        microbilogy = Microbiology(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, microbilogy.clean)
