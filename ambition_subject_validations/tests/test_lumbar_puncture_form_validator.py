da235214ba2d59e71badcfc0c49ddf0109af900c
kmotingwa <kmotingwa@bhp.org.bw>
2017-06-07 15:24:44 +0200

    fix lumber_puncture test

--- ambition_subject_validations/tests/test_lumbar_puncture_form_validator.py
+++ ambition_subject_validations/tests/test_lumbar_puncture_form_validator.py
@@ -1,6 +1,5 @@
-from django import forms
 from django.core.exceptions import ValidationError
-from django.test import TestCase, tag
+from django.test import TestCase
 
 from edc_constants.constants import YES, NO
 
@@ -9,71 +8,57 @@ from ..form_validators import LumbarPunctureCSFFormValidator
 
 class TestLumbarPunctureFormValidator(TestCase):
 
-    @tag('1')
     def test_csf_culture_yes(self):
         cleaned_data = {'csf_culture': YES}
         form = LumbarPunctureCSFFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form.clean)
-
-    @tag('1')
-    def test_csf_culture_no_raises(self):
-        cleaned_data = {'csf_culture': NO}
-        form = LumbarPunctureCSFFormValidator(cleaned_data=cleaned_data)
-        try:
-            form.clean()
-        except forms.ValidationError:
-            self.fail(f'ValidationError unexpectedly raised.')
 
     def test_csf_culture_no(self):
         options = {
             'csf_culture': NO,
             'other_csf_culture': YES}
         form = LumbarPunctureCSFFormValidator(cleaned_data=options)
-        self.assertRaisesMessage(
-            ValidationError, 'This field is not required.')
+        self.assertRaises(ValidationError, form.clean)
 
     def test_if_csf_wbc_cell_count_greater_than_zero(self):
-        """Assert that the csf wbc count greater or equal to 0
-        and less than 3.
+        """Assert that the csf wbc count greater than 0.
         """
         options = {
             'csf_wbc_cell_count': 1}
         form = LumbarPunctureCSFFormValidator(cleaned_data=options)
-        self.assertTrue(form.clean())
+        self.assertRaises(ValidationError, form.clean)
 
-    def test_if_csf_wbc_cell_count_less_than_zero(self):
+    def test_if_csf_wbc_cell_count_less_than_three(self):
         options = {
-            'csf_wbc_cell_count': 0}
+            'csf_culture': YES,
+            'csf_wbc_cell_count': 3}
         form = LumbarPunctureCSFFormValidator(cleaned_data=options)
-        self.assertRaisesMessage(
-            ValidationError, 'This field is not required.')
+        self.assertRaises(ValidationError, form.clean)
 
     def test_differential_lymphocyte_count_not_required(self):
         options = {
-            'csf_wbc_cell_count': 0,
-            'differential_lymphocyte_count': None,
-            'differential_neutrophil_count': 5}
+            'csf_wbc_cell_count': 1,
+            'differential_lymphocyte_count': None}
         form = LumbarPunctureCSFFormValidator(cleaned_data=options)
-        self.assertTrue(form.clean())
+        self.assertRaises(ValidationError, form.clean)
 
     def test_differential_lymphocyte_count_greater_than_zero(self):
         options = {
+            'csf_wbc_cell_count': 1,
             'differential_lymphocyte_count': 4}
         form = LumbarPunctureCSFFormValidator(cleaned_data=options)
-        self.assertRaisesMessage(
-            ValidationError, 'This field is required.')
+        self.assertRaises(ValidationError, form.clean)
 
     def differential_neutrophil_count_not_required(self):
         options = {
-            'csf_wbc_cell_count': 0,
-            'differential_lymphocyte_count': 4,
+            'csf_wbc_cell_count': 2,
             'differential_neutrophil_count': None}
         form = LumbarPunctureCSFFormValidator(cleaned_data=options)
         self.assertTrue(form.clean())
 
     def test_neutrophil_count_greater_than_zero(self):
         options = {
-            'differential_neutrophil_count': 4}
+            'csf_wbc_cell_count': 1,
+            'differential_neutrophil_count': 5}
         form = LumbarPunctureCSFFormValidator(cleaned_data=options)
-        self.assertRaisesMessage(
-            ValidationError, 'This field is required.')
+        self.assertRaises(ValidationError, form.clean)
