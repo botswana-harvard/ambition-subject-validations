93935caccde72dd7c99810280b9dcbe99da0b9aa
Gofaone Magodi <magodign@magodign.local>
2017-06-08 10:08:19 +0200

    complete study term tests

--- ambition_subject_validations/form_validators/study_termination_conclusion_form_validator.py
+++ ambition_subject_validations/form_validators/study_termination_conclusion_form_validator.py
@@ -1,5 +1,5 @@
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import YES
+from edc_constants.constants import YES, OTHER
 
 
 class StudyTerminationConclusionFormValidator(FormValidator):
@@ -21,42 +21,31 @@ class StudyTerminationConclusionFormValidator(FormValidator):
         self.required_if(
             'withdrawal_of_subject_consent',
             field='termination_reason',
-            field_required='consent_withdrawal_reason',
-        )
-#
-#         self.required_if(
-#             YES,
-#             field='rifampicin_started_since_week4',
-#             field_required='rifampicin_started_study_day')
-#
-#         self.required_if(
-#             YES,
-#             field='willing_to_complete_10W_FU',
-#             field_required='date_willing_to_complete')
-#
-#         self.required_if(
-#             YES,
-#             field='willing_to_complete_centre',
-#             field_required='date_willing_to_complete')
-#
-#         self.required_if(
-#             YES,
-#             field='late_protocol_exclusion',
-#             field_required='rifampicin_started')
-#
-#         self.required_if(
-#             OTHER,
-#             field='first_line_regimen_patients',
-#             field_required='first_line_regimen_patients_other')
-#
-#         self.required_if(
-#             OTHER,
-#             field='second_line_regimen_patients',
-#             field_required='second_line_regimen_patients_other')
-#
-#         self.required_if(
-#             NOT_APPLICABLE,
-#             field='date_arvs_started_or_switched',
-#             field_required='arvs_delay_reason')
+            field_required='consent_withdrawal_reason')
+
+        self.required_if(
+            YES,
+            field='willing_to_complete_10W_FU',
+            field_required='date_willing_to_complete')
+
+        self.required_if(
+            YES,
+            field='willing_to_complete_centre',
+            field_required='date_willing_to_complete')
+
+        self.required_if(
+            OTHER,
+            field='first_line_regimen_patients',
+            field_required='first_line_regimen_patients_other')
+
+        self.required_if(
+            OTHER,
+            field='second_line_regimen_patients',
+            field_required='second_line_regimen_patients_other')
+
+        self.required_if(
+            None,
+            field='date_arvs_started_or_switched',
+            field_required='arvs_delay_reason')
 
         return self.cleaned_data
