9330874adeefdfc0f12d4e705e72cc9c87a8527b
kmotingwa <kmotingwa@bhp.org.bw>
2017-06-09 15:18:04 +0200

    test on patients history

--- ambition_subject_validations/form_validators/patient_history_form_validator.py
+++ ambition_subject_validations/form_validators/patient_history_form_validator.py
@@ -1,4 +1,5 @@
 from edc_base.modelform_validators import FormValidator
+from edc_constants.constants import YES, NO, OTHER
 
 
 class PatientHistoryFormValidator(FormValidator):
@@ -9,59 +10,59 @@ class PatientHistoryFormValidator(FormValidator):
         self.required_if_true(
             condition=condition, field_required='first_line_choice',
             cleaned_data=self.cleaned_data)
-#
-#         self.m2m_required_if(response='focal_neurologic_deficit',
-#                              field='focal_neurologic_deficit',
-#                              m2m_field='neurological')
 
-#         self.required_if(
-#             YES,
-#             field='med_history',
-#             field_required='tb_site')
-#
-#         self.required_if(
-#             YES,
-#             field='tb_treatment',
-#             field_required='taking_rifampicin')
-#
-#         self.required_if(
-#             YES,
-#             field='taking_rifampicin',
-#             field_required='rifampicin_started_date')
-#
-#         self.required_if(
-#             YES,
-#             field='previous_infection',
-#             field_required='infection_date')
-#
-#         self.required_if(
-#             YES,
-#             field='previous_infection',
-#             field_required='previous_infection_specify')
-#
-#         self.required_if(
-#             YES,
-#             field='taking_arv',
-#             field_required='arv_date')
-#
-#         self.required_if(
-#             OTHER,
-#             field='first_line_arvs',
-#             field_required='first_line_arvs_other')
-#
-#         self.required_if(
-#             OTHER,
-#             field='second_line_arvs',
-#             field_required='second_line_arvs_other')
-#
-#         self.required_if(
-#             NO,
-#             field='patient_adherence',
-#             field_required='last_dose')
-#
-#         self.required_if(
-#             YES,
-#             field='other_medications',
-#             field_required='specify_medications')
-#
+        self.m2m_required_if(response='focal_neurologic_deficit',
+                             field='focal_neurologic_deficit',
+                             m2m_field='neurological')
+
+        self.required_if(
+            YES,
+            field='med_history',
+            field_required='tb_site')
+
+        self.required_if(
+            YES,
+            field='tb_treatment',
+            field_required='taking_rifampicin')
+
+        self.required_if(
+            YES,
+            field='taking_rifampicin',
+            field_required='rifampicin_started_date')
+
+        self.required_if(
+            YES,
+            field='previous_infection',
+            field_required='infection_date')
+
+        self.required_if(
+            YES,
+            field='previous_infection',
+            field_required='previous_infection_specify')
+
+        self.required_if(
+            YES,
+            field='taking_arv',
+            field_required='arv_date')
+
+        self.required_if(
+            OTHER,
+            field='first_line_arvs',
+            field_required='first_line_arvs_other')
+
+        self.required_if(
+            OTHER,
+            field='second_line_arvs',
+            field_required='second_line_arvs_other')
+
+        self.required_if(
+            NO,
+            field='patient_adherence',
+            field_required='last_dose')
+
+        self.required_if(
+            YES,
+            field='other_medications',
+            field_required='specify_medications')
+
         return self.cleaned_data
--- ambition_subject_validations/tests/test_patient_history_form_validator.py
+++ ambition_subject_validations/tests/test_patient_history_form_validator.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from edc_constants.constants import YES
+
 from ..form_validators import PatientHistoryFormValidator
 
 
@@ -9,10 +11,9 @@ class TestPatientHistoryFormValidator(TestCase):
     def test_first_line_choice_yes(self):
         """Assert that the first line choice is within the first_line_arvs
         """
-        options = {'first_line_arvs': 'AZT + 3-TC + either EFV or NVP or DTG',
-                   'first_line_choice': 'EFV'}
+        options = {'first_line_choice': 'EFV'}
         form = PatientHistoryFormValidator(cleaned_data=options)
-        self.assertTrue(form.clean())
+        self.assertRaises(ValidationError, form.clean)
 
     def test_first_line_choice_no(self):
         """Assert that the first line choice is not provided
@@ -22,12 +23,12 @@ class TestPatientHistoryFormValidator(TestCase):
         form = PatientHistoryFormValidator(cleaned_data=options)
         self.assertRaises(ValidationError, form.clean)
 
-    def test_if_focal_neurological_deficit(self):
+    def test_if_focal_neurological_deficit_yes(self):
         """Assert that patient has focal neurological deficit
         """
-        options = {'focal_neurologic_deficit': 'meningismus'}
+        options = {'neurological': YES, 'focal_neurologic_deficit': 'meningismus', 'first_line_choice': 'EFV'}
         form = PatientHistoryFormValidator(cleaned_data=options)
-        self.assertTrue(form.clean())
+        self.assertRaises(ValidationError, form.clean)
 
     def test_if_focal_neurological_deficit_none(self):
         options = {'first_line_arvs': 'AZT + 3-TC + either EFV or NVP or DTG',
