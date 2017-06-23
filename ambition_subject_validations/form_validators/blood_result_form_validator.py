1cb8b76124add928c7b446a429191bef404fe1db
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-06-23 11:20:17 +0200

    add decimal fields validations

--- ambition_subject_validations/form_validators/blood_result_form_validator.py
+++ ambition_subject_validations/form_validators/blood_result_form_validator.py
@@ -1,3 +1,5 @@
+from django.forms import ValidationError
+
 from edc_base.modelform_validators import FormValidator
 from edc_constants.constants import NO
 
@@ -11,4 +13,27 @@ class BloodResultFormValidator(FormValidator):
             field='are_results_normal',
             field_required='abnormal_results_in_ae_range')
 
+        creatinine_field = self.cleaned_data.get('creatinine')
+        creatinine_unit = self.cleaned_data.get('creatinine_unit')
+
+        self.validate_non_decimal_values(field=creatinine_field,
+                                         field_unit=creatinine_unit)
+
+        magnesium_field = self.cleaned_data.get('magnesium')
+        magnesium_unit = self.cleaned_data.get('magnesium_unit')
+
+        self.validate_non_decimal_values(field=magnesium_field,
+                                         field_unit=magnesium_unit)
+
+        urea_field = self.cleaned_data.get('urea')
+        urea_unit = self.cleaned_data.get('urea_unit')
+
+        self.validate_non_decimal_values(field=urea_field,
+                                         field_unit=urea_unit)
+
         return self.cleaned_data
+
+    def validate_non_decimal_values(self, field=None, field_unit=None):
+        if (field_unit == 'mmol/L' and str(field).split('.')[1] != '00'):
+            raise ValidationError({
+                field: 'Please provide a whole number for mmol/L units'})
