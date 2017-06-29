d54b5cfc9f354b5dc275875de59e8e90fc736b74
Leslie <leslie.kep@gmail.com>
2017-06-29 11:40:19 +0200

    flake8

--- ambition_subject_validations/tests/test_death_form_validator.py
+++ ambition_subject_validations/tests/test_death_form_validator.py
@@ -1,33 +1,33 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-  
+
 from edc_constants.constants import YES, NO
-  
+
 from ..form_validators import DeathFormValidator
-  
-  
+
+
 class TestDeathFormValidations(TestCase):
-  
+
     def test_death_inpatient_yes(self):
         cleaned_data = {
             'death_as_inpatient': YES, 'cause_of_death_study_doctor_opinion': None}
         form = DeathFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form.clean)
-        
+
     def test_death_inpatient_no(self):
         cleaned_data = {
             'death_as_inpatient': NO,
-            'cause_of_death_study_doctor_opinion':'cryptococcal_meningitis'}
+            'cause_of_death_study_doctor_opinion': 'cryptococcal_meningitis'}
         form = DeathFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form.clean)
-          
+
     def test_cause_of_death_study_doc_opinion_yes(self):
         cleaned_data = {
             'cause_of_death_study_doctor_opinion': YES,
             'cause_tb_study_doctor_opinion': None}
         form = DeathFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form.clean)
-        
+
     def test_cause_of_death_study_doc_opinion_no(self):
         cleaned_data = {
             'cause_of_death_study_doctor_opinion': NO,
