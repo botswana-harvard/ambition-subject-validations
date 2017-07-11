cecd5a4186dd1fa2fafa49f8fb2961ccdd397e72
aseokana <aseokana@bhp.org.bw>
2017-07-11 11:10:55 +0200

    tests and form validators for the deviation_or_violation field

--- ambition_subject_validations/form_validators/protocol_deviation_violation_form_validator.py
+++ ambition_subject_validations/form_validators/protocol_deviation_violation_form_validator.py
@@ -1,10 +1,23 @@
 from edc_base.modelform_validators import FormValidator
 from edc_constants.constants import YES, OTHER
+from ambition_subject.constants import DEVIATION
 
 
 class ProtocolDeviationViolationFormValidator(FormValidator):
 
     def clean(self):
+        field_required_list = ['date_violation_datetime',
+                               'protocol_violation_type',
+                               'other_protocol_violation_type',
+                               'violation_description',
+                               'violation_reason']
+        for field_required in field_required_list:
+            self.not_required_if(
+                DEVIATION,
+                field='deviation_or_violation',
+                field_required=field_required
+            )
+
         self.required_if(
             YES,
             field='participant_safety_impact',
--- ambition_subject_validations/tests/test_protocol_deviation_violation_form_validator.py
+++ ambition_subject_validations/tests/test_protocol_deviation_violation_form_validator.py
@@ -1,13 +1,116 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.test import TestCase, tag
+from edc_base.utils import get_utcnow
 
 from edc_constants.constants import YES, NO, NOT_APPLICABLE, OTHER
+from ambition_subject.constants import DEVIATION, VIOLATION
 
 from ..form_validators import ProtocolDeviationViolationFormValidator
+from pprint import pprint
 
 
 class TestProtocolDeviationViolationFormValidator(TestCase):
+
+    def test_date_violation_datetime_deviation(self):
+        """ date_violation_datetime is not required if it's
+        a protocol deviation
+         """
+        cleaned_data = {'deviation_or_violation': DEVIATION,
+                        'date_violation_datetime': get_utcnow()}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_date_violation_datetime_deviation_violation(self):
+        """ date_violation_datetime is required if it's
+        a protocol violation
+         """
+        cleaned_data = {'deviation_or_violation': VIOLATION,
+                        'date_violation_datetime': None}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_protocol_violation_type_deviation(self):
+        """ protocol_violation_type is not required if it's
+        a protocol deviation
+         """
+        cleaned_data = {'deviation_or_violation': DEVIATION,
+                        'protocol_violation_type': 'type'}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_protocol_violation_type_violation(self):
+        """ protocol_violation_type is required if it's
+        a protocol violation
+         """
+        cleaned_data = {'deviation_or_violation': VIOLATION,
+                        'protocol_violation_type': None}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_other_protocol_violation_type_deviation(self):
+        """ other_protocol_violation_type is not required if it's
+        a protocol deviation
+         """
+        cleaned_data = {'deviation_or_violation': DEVIATION,
+                        'other_protocol_violation_type': 'other type'}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_other_protocol_violation_type_violation(self):
+        """ other_protocol_violation_type is required if it's
+        a protocol violation
+         """
+        cleaned_data = {'deviation_or_violation': VIOLATION,
+                        'other_protocol_violation_type': None}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_violation_description_deviation(self):
+        """ violation_description is not required if it's
+        a protocol deviation
+         """
+        cleaned_data = {'deviation_or_violation': DEVIATION,
+                        'violation_description': 'description'}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_violation_description_violation(self):
+        """ violation_description is required if it's
+        a protocol violation
+         """
+        cleaned_data = {'deviation_or_violation': VIOLATION,
+                        'violation_description': None}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_violation_reason_deviation(self):
+        """ violation_reason is not required if it's
+        a protocol deviation
+         """
+        cleaned_data = {'deviation_or_violation': DEVIATION,
+                        'violation_reason': 'reason'}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
+
+    def test_violation_reason_deviation_violation(self):
+        """ violation_reason is required if it's
+        a protocol violation
+         """
+        cleaned_data = {'deviation_or_violation': VIOLATION,
+                        'violation_reason': None}
+        form_validator = ProtocolDeviationViolationFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.clean)
 
     def test_yes_participant_safety_impact_none_details(self):
         """ Asserts participant_safety_impact has valid
