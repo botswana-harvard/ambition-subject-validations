28c6d798e95a6c3277a2edd56fa29590b4a631f2
Itumeleng Mosweu <imosweuk@gmail.com>
2017-07-12 14:13:00 +0200

    fix followup validations and tests

--- ambition_subject_validations/form_validators/follow_up_form_validator.py
+++ ambition_subject_validations/form_validators/follow_up_form_validator.py
@@ -5,53 +5,6 @@ from edc_constants.constants import YES, NO
 class FollowUpFormValidator(FormValidator):
 
     def clean(self):
-        self.required_if(
-            YES,
-            field='tb_pulmonary_dx',
-            field_required='tb_pulmonary_dx_date',
-        )
-
-        self.required_if(
-            YES,
-            field='extra_pulmonary_tb_dx',
-            field_required='extra_tb_pulmonary_dx_date',
-        )
-
-        self.required_if(
-            YES,
-            field='kaposi_sarcoma_dx',
-            field_required='kaposi_sarcoma_dx_date',
-        )
-
-        self.required_if(
-            YES,
-            field='malaria_dx',
-            field_required='malaria_dx_date',
-        )
-
-        self.required_if(
-            YES,
-            field='bacteraemia_dx',
-            field_required='bacteraemia_dx_date',
-        )
-
-        self.required_if(
-            YES,
-            field='pneumonia_dx',
-            field_required='pneumonia_dx_date',
-        )
-
-        self.required_if(
-            YES,
-            field='diarrhoeal_wasting_dx',
-            field_required='diarrhoeal_wasting_dx_date',
-        )
-
-        self.required_if(
-            YES,
-            field='other_significant_new_diagnosis',
-            field_required='diagnosis_date',
-        )
 
         self.required_if(
             NO,
