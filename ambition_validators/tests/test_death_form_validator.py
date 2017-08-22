ae981a35a7b4c61cb1392e837984c2cb4cd7da2c
Itumeleng Mosweu <imosweuk@gmail.com>
2017-08-22 09:39:06 +0200

    update death form validator tests

--- ambition_validators/tests/test_death_form_validator.py
+++ ambition_validators/tests/test_death_form_validator.py
@@ -1,68 +1,48 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.test import TestCase, tag
 from edc_constants.constants import OTHER
 
 from ..form_validators import DeathFormValidator
 
 
+@tag("df")
 class TestDeathFormValidations(TestCase):
-
-    def test_cause_of_death_study_doc_opinion_yes(self):
-        cleaned_data = {
-            'cause_of_death_study_doctor_opinion': 'TB',
-            'cause_tb_study_doctor_opinion': None}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form.clean)
-
-    def test_cause_of_death_study_doc_opinion_no(self):
-        cleaned_data = {
-            'cause_of_death_study_doctor_opinion': 'TB',
-            'cause_tb_study_doctor_opinion': 'meningitis'}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
-        try:
-            form.clean()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_cause_of_death_study_doc_opinion_other(self):
         cleaned_data = {
             'cause_of_death_study_doctor_opinion': OTHER,
             'cause_other_study_doctor_opinion': None}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form.clean)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn(
+            'cause_other_study_doctor_opinion', form_validator._errors)
 
     def test_cause_of_death_study_doc_opinion_other_no(self):
         cleaned_data = {
             'cause_of_death_study_doctor_opinion': OTHER,
             'cause_other_study_doctor_opinion': 'blah'}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
         try:
-            form.clean()
+            form_validator.validate()
         except forms.ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
-    def test_cause_of_death_study_doc_opinion_death_narrative_none(self):
-        cleaned_data = {
-            'cause_of_death_study_doctor_opinion': OTHER,
-            'cause_other_study_doctor_opinion': None}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form.clean)
-
-    def test_cause_of_death_study_doctor_tb_no_site_specified_invalid(self):
+    def test_cause_of_death_study_doc_opinion_yes(self):
         cleaned_data = {
             'cause_of_death_study_doctor_opinion': 'TB',
             'cause_tb_study_doctor_opinion': None}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form.clean)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('cause_tb_study_doctor_opinion', form_validator._errors)
 
-    def test_cause_of_death_study_doctor_tb_site_specified_valid(self):
+    def test_cause_of_death_study_doc_opinion_no(self):
         cleaned_data = {
             'cause_of_death_study_doctor_opinion': 'TB',
             'cause_tb_study_doctor_opinion': 'meningitis'}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
         try:
-            form.clean()
+            form_validator.validate()
         except forms.ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
@@ -70,16 +50,17 @@ class TestDeathFormValidations(TestCase):
         cleaned_data = {
             'cause_of_death_tmg1_opinion': 'TB',
             'cause_tb_tmg1_opinion': None}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form.clean)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('cause_tb_tmg1_opinion', form_validator._errors)
 
     def test_cause_of_death_study_tmg1_tb_site_specified_valid(self):
         cleaned_data = {
             'cause_of_death_tmg1_opinion': 'TB',
             'cause_tb_tmg1_opinion': 'meningitis'}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
         try:
-            form.clean()
+            form_validator.validate()
         except forms.ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
@@ -87,15 +68,16 @@ class TestDeathFormValidations(TestCase):
         cleaned_data = {
             'cause_of_death_tmg2_opinion': 'TB',
             'cause_tb_tmg2_opinion': None}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form.clean)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('cause_tb_tmg2_opinion', form_validator._errors)
 
     def test_cause_of_death_study_tmg2_tb_site_specified_valid(self):
         cleaned_data = {
             'cause_of_death_tmg2_opinion': 'TB',
             'cause_tb_tmg2_opinion': 'meningitis'}
-        form = DeathFormValidator(cleaned_data=cleaned_data)
+        form_validator = DeathFormValidator(cleaned_data=cleaned_data)
         try:
-            form.clean()
+            form_validator.validate()
         except forms.ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
