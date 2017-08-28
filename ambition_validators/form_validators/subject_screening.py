ae9608ba6402ece3fe60f4d58291be1066639cd1
aseokana <aseokana@bhp.org.bw>
2017-08-28 15:01:11 +0200

    adding preg_test_date validator

--- ambition_validators/form_validators/subject_screening.py
+++ ambition_validators/form_validators/subject_screening.py
@@ -1,11 +1,17 @@
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import FEMALE
+from edc_constants.constants import MALE, NOT_APPLICABLE
 
 
 class SubjectScreeningFormValidator(FormValidator):
 
     def clean(self):
-        self.required_if(
-            FEMALE,
+        self.not_required_if(
+            MALE,
             field='gender',
-            field_required='pregnancy_or_lactation')
+            field_required='pregnancy')
+
+        self.not_required_if(
+            NOT_APPLICABLE,
+            field='pregnancy',
+            field_required='preg_test_date'
+        )
--- ambition_validators/tests/test_subject_screening.py
+++ ambition_validators/tests/test_subject_screening.py
@@ -1,24 +1,40 @@
 from django import forms
-from django.test import TestCase
+from django.test import TestCase, tag
 
-from edc_constants.constants import YES, FEMALE
-from edc_base.modelform_validators import REQUIRED_ERROR
+from edc_base.utils import get_utcnow
+from edc_constants.constants import MALE, YES, NOT_APPLICABLE
+from edc_base.modelform_validators import NOT_REQUIRED_ERROR
 
 from ..form_validators import SubjectScreeningFormValidator
 
 
 class TestSubjectScreeningFormValidator(TestCase):
-
+    @tag('999')
     def test_gender(self):
         options = {
-            'gender': FEMALE,
-            'pregnancy_or_lactation': None}
+            'gender': MALE,
+            'pregnancy': YES}
         form_validator = SubjectScreeningFormValidator(
             cleaned_data=options)
         try:
             form_validator.validate()
         except forms.ValidationError:
             pass
-        self.assertIn('pregnancy_or_lactation',
+        self.assertIn('pregnancy',
                       form_validator._errors)
-        self.assertIn(REQUIRED_ERROR, form_validator._error_codes)
+        self.assertIn(NOT_REQUIRED_ERROR, form_validator._error_codes)
+
+    @tag('999')
+    def test_preg_test_date(self):
+        options = {
+            'pregnancy': NOT_APPLICABLE,
+            'preg_test_date': get_utcnow}
+        form_validator = SubjectScreeningFormValidator(
+            cleaned_data=options)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn('preg_test_date',
+                      form_validator._errors)
+        self.assertIn(NOT_REQUIRED_ERROR, form_validator._error_codes)
