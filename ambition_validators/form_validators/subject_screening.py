6e8db89a18187b3ae29a843f978568f52d357b57
Itumeleng Mosweu <imosweuk@gmail.com>
2017-08-30 12:55:54 +0200

    fix screening validations and tests

--- ambition_validators/form_validators/subject_screening.py
+++ ambition_validators/form_validators/subject_screening.py
@@ -1,11 +1,21 @@
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import FEMALE, YES
+from edc_constants.constants import FEMALE, YES, NOT_APPLICABLE
 
 
 class SubjectScreeningFormValidator(FormValidator):
 
     def clean(self):
-        self.required_if(
+        self.applicable_if(
             FEMALE,
             field='gender',
-            field_required='pregnancy_or_lactation')
+            field_applicable='pregnancy')
+
+        self.not_required_if(
+            NOT_APPLICABLE,
+            field='pregnancy',
+            field_required='preg_test_date')
+
+        self.applicable_if(
+            FEMALE,
+            field='gender',
+            field_applicable='breast_feeding')
--- ambition_validators/tests/test_subject_screening.py
+++ ambition_validators/tests/test_subject_screening.py
@@ -1,24 +1,52 @@
 from django import forms
 from django.test import TestCase
 
-from edc_constants.constants import YES, FEMALE
-from edc_base.modelform_validators import REQUIRED_ERROR
+from edc_constants.constants import YES, FEMALE, NOT_APPLICABLE, NO
+from edc_base.modelform_validators import APPLICABLE_ERROR, REQUIRED_ERROR
 
 from ..form_validators import SubjectScreeningFormValidator
 
 
 class TestSubjectScreeningFormValidator(TestCase):
 
-    def test_gender(self):
+    def test_female_pregnancy_not_applicable_invalid(self):
         options = {
             'gender': FEMALE,
-            'pregnancy_or_lactation': None}
+            'pregnancy': NOT_APPLICABLE}
         form_validator = SubjectScreeningFormValidator(
             cleaned_data=options)
         try:
             form_validator.validate()
         except forms.ValidationError:
             pass
-        self.assertIn('pregnancy_or_lactation',
+        self.assertIn('pregnancy',
+                      form_validator._errors)
+        self.assertIn(APPLICABLE_ERROR, form_validator._error_codes)
+
+    def test_pregnancy_no_preg_test_date_invalid(self):
+        options = {
+            'pregnancy': NO,
+            'preg_test_date': None}
+        form_validator = SubjectScreeningFormValidator(
+            cleaned_data=options)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn('preg_test_date',
                       form_validator._errors)
         self.assertIn(REQUIRED_ERROR, form_validator._error_codes)
+
+    def test_female_breast_feeding_not_applicable_invalid(self):
+        options = {
+            'gender': FEMALE,
+            'breast_feeding': NOT_APPLICABLE}
+        form_validator = SubjectScreeningFormValidator(
+            cleaned_data=options)
+        try:
+            form_validator.validate()
+        except forms.ValidationError:
+            pass
+        self.assertIn('breast_feeding',
+                      form_validator._errors)
+        self.assertIn(APPLICABLE_ERROR, form_validator._error_codes)
