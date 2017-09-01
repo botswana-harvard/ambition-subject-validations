d0f6854667c5017278130b8b0147779e94467ec3
Leslie <leslie.kep@gmail.com>
2017-09-01 10:18:40 +0200

    update screening validations

--- ambition_validators/form_validators/subject_screening.py
+++ ambition_validators/form_validators/subject_screening.py
@@ -1,21 +1,31 @@
+from django.forms import forms
 from edc_base.modelform_validators import FormValidator
-from edc_constants.constants import FEMALE, YES, NOT_APPLICABLE
+from edc_base.modelform_validators.base_form_validator import REQUIRED_ERROR
+from edc_constants.constants import FEMALE, YES, NO, MALE, NOT_APPLICABLE
 
 
 class SubjectScreeningFormValidator(FormValidator):
 
     def clean(self):
-        self.applicable_if(
-            FEMALE,
-            field='gender',
-            field_applicable='pregnancy')
 
-        self.not_required_if(
-            NOT_APPLICABLE,
-            field='pregnancy',
-            field_required='preg_test_date')
+        condition = (
+            self.cleaned_data.get('gender') == FEMALE and
+            self.cleaned_data.get('pregnancy') in [YES, NO])
 
-        self.applicable_if(
-            FEMALE,
-            field='gender',
-            field_applicable='breast_feeding')
+        self.required_if_true(
+            condition=condition, field_required='preg_test_date')
+
+        if (self.cleaned_data.get('gender') == MALE and
+                self.cleaned_data.get('pregnancy') not in [NOT_APPLICABLE]):
+            message = {
+                'pregnancy': 'This field is not applicable'}
+            self._errors.update(message)
+            self._error_codes.append(REQUIRED_ERROR)
+            raise forms.ValidationError(message, code=REQUIRED_ERROR)
+
+        if (self.cleaned_data.get('gender') == MALE):
+            message = {
+                'breast_feeding': 'This field is not applicable'}
+            self._errors.update(message)
+            self._error_codes.append(REQUIRED_ERROR)
+            raise forms.ValidationError(message, code=REQUIRED_ERROR)
