c73d892910cc74117fd0d9233658a03516cf6022
Leslie <leslie.kep@gmail.com>
2017-09-05 09:53:19 +0200

    microbiology validations

--- ambition_validators/form_validators/microbiology.py
+++ ambition_validators/form_validators/microbiology.py
@@ -5,50 +5,81 @@ from edc_constants.constants import POS, YES, OTHER
 class MicrobiologyFormValidator(FormValidator):
 
     def clean(self):
+
         self.required_if(
             YES,
             field='urine_culture_performed',
-            field_required='urine_culture_results')
+            field_required='urine_taken_date')
+
+        self.applicable_if(
+            YES,
+            field='urine_culture_performed',
+            field_applicable='urine_culture_results')
+
+        self.applicable_if(
+            POS,
+            field='urine_culture_results',
+            field_applicable='urine_culture_organism')
+
+        self.validate_other_specify(
+            field='urine_culture_organism',
+            other_specify_field='urine_culture_organism_other',
+            other_stored_value=OTHER)
+
+        self.applicable_if(
+            YES,
+            field='blood_culture_performed',
+            field_applicable='blood_culture_results')
 
         self.required_if(
             POS,
-            field='urine_culture_results',
-            field_required='urine_culture_organism')
+            field='blood_culture_results',
+            field_required='blood_taken_date')
 
         self.required_if(
-            OTHER,
-            field='urine_culture_results',
-            field_required='urine_culture_organism_other')
+            POS,
+            field='blood_culture_results',
+            field_required='day_blood_taken')
+
+        self.applicable_if(
+            POS,
+            field='blood_culture_results',
+            field_applicable='blood_culture_organism')
+
+        self.validate_other_specify(
+            field='blood_culture_organism',
+            other_specify_field='blood_culture_organism_other',
+            other_stored_value=OTHER)
+
+        self.applicable_if_true(
+            condition=self.cleaned_data.get(
+                'blood_culture_organism') == 'bacteria',
+            field_applicable='bacteria_identified')
+
+        self.validate_other_specify(
+            field='bacteria_identified',
+            other_specify_field='bacteria_identified_other',
+            other_stored_value=OTHER)
 
         self.required_if(
             YES,
-            field='blood_culture_performed',
-            field_required='blood_culture_results')
+            field='sputum_afb_performed',
+            field_required='sputum_afb_date')
+
+        self.applicable_if(
+            YES,
+            field='sputum_afb_performed',
+            field_applicable='sputum_results_afb')
 
         self.required_if(
-            POS,
-            field='blood_culture_results',
-            field_required='date_blood_taken')
+            YES,
+            field='sputum_performed',
+            field_required='sputum_taken_date')
 
-        self.required_if(
-            POS,
-            field='blood_culture_results',
-            field_required='blood_culture_organism')
-
-        self.required_if(
-            OTHER,
-            field='blood_culture_organism',
-            field_required='blood_culture_organism_other')
-
-        self.required_if(
-            'bacteria',
-            field='blood_culture_organism',
-            field_required='bacteria_identified')
-
-        self.required_if(
-            OTHER,
-            field='bacteria_identified',
-            field_required='bacteria_identified_other')
+        self.applicable_if(
+            YES,
+            field='sputum_performed',
+            field_applicable='sputum_results_culture')
 
         self.required_if(
             POS,
@@ -57,25 +88,40 @@ class MicrobiologyFormValidator(FormValidator):
 
         self.required_if(
             YES,
+            field='sputum_genexpert_performed',
+            field_required='sputum_genexpert_date')
+
+        self.required_if(
+            POS,
+            field='sputum_genexpert_performed',
+            field_required='sputum_results_positive')
+
+        self.applicable_if(
+            YES,
+            field='sputum_genexpert_performed',
+            field_applicable='sputum_result_genexpert')
+
+        self.applicable_if(
+            YES,
             field='tissue_biopsy_taken',
-            field_required='tissue_biopsy_results')
+            field_applicable='tissue_biopsy_results')
 
         self.required_if(
             POS,
             field='tissue_biopsy_results',
-            field_required='date_biopsy_taken')
+            field_required='biopsy_date')
 
         self.required_if(
-            POS,
-            field='tissue_biopsy_results',
+            YES,
+            field='tissue_biopsy_taken',
             field_required='day_biopsy_taken')
 
-        self.required_if(
+        self.applicable_if(
             POS,
             field='tissue_biopsy_results',
-            field_required='tissue_biopsy_organism')
+            field_applicable='tissue_biopsy_organism')
 
-        self.required_if(
-            OTHER,
+        self.validate_other_specify(
             field='tissue_biopsy_organism',
-            field_required='tissue_biopsy_organism_other')
+            other_specify_field='tissue_biopsy_organism_other',
+            other_stored_value=OTHER)
--- ambition_validators/tests/test_microbiology_form_validator.py
+++ ambition_validators/tests/test_microbiology_form_validator.py
@@ -11,9 +11,11 @@ class TestMicrobiologyFormValidator(TestCase):
 
     def test_urine_culture_performed_yes_require_urine_culture_results(self):
         cleaned_data = {'urine_culture_performed': YES,
-                        'urine_culture_results': None}
+                        'urine_taken_date': get_utcnow(),
+                        'urine_culture_results': NOT_APPLICABLE}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('urine_culture_results', form_validator._errors)
 
     def test_urine_culture_performed_no_require_urine_culture_results(self):
         cleaned_data = {'urine_culture_performed': NO,
@@ -27,20 +29,19 @@ class TestMicrobiologyFormValidator(TestCase):
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
 
-    def test_urine_culture_performed_yes_given_urine_culture_results(self):
-        cleaned_data = {'urine_culture_performed': YES,
-                        'urine_culture_results': 'no_growth'}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+#     def test_urine_culture_performed_yes_given_urine_culture_results(self):
+#         cleaned_data = {'urine_culture_performed': YES,
+#                         'urine_culture_results': 'no_growth'}
+#         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
+#         self.assertRaises(ValidationError, form_validator.validate)
+#         self.assertIn('urine_culture_results', form_validator._errors)
 
     def test_pos_urine_culture_results_none_urine_culture_organism(self):
         cleaned_data = {'urine_culture_results': POS,
-                        'urine_culture_organism': None}
+                        'urine_culture_organism': NOT_APPLICABLE}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('urine_culture_organism', form_validator._errors)
 
     def test_pos_urine_results_na_urine_culture_organism(self):
         cleaned_data = {'urine_culture_results': POS,
@@ -58,22 +59,25 @@ class TestMicrobiologyFormValidator(TestCase):
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_other_urine_culture_results_require_urine_organism_other(self):
-        cleaned_data = {'urine_culture_results': OTHER,
+        cleaned_data = {'urine_culture_organism': OTHER,
                         'urine_culture_organism_other': None}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('urine_culture_organism_other', form_validator._errors)
 
-    def test_other_urine_culture_results_na_urine_organism_other(self):
-        cleaned_data = {'urine_culture_results': OTHER,
-                        'urine_culture_organism_other': NOT_APPLICABLE}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.validate)
+#     def test_other_urine_culture_results_na_urine_organism_other(self):
+#         cleaned_data = {'urine_culture_organism': OTHER,
+#                         'urine_culture_organism_other': NOT_APPLICABLE}
+#         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
+#         self.assertRaises(ValidationError, form_validator.validate)
+#         self.assertIn('urine_culture_organism', form_validator._errors)
 
     def test_yes_blood_culture_performed_none_blood_culture_results(self):
         cleaned_data = {'blood_culture_performed': YES,
-                        'blood_culture_results': None}
+                        'blood_culture_results': NOT_APPLICABLE}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('blood_culture_results', form_validator._errors)
 
     def test_yes_blood_culture_performed_with_blood_culture_results(self):
         cleaned_data = {'blood_culture_performed': YES,
@@ -86,12 +90,10 @@ class TestMicrobiologyFormValidator(TestCase):
 
     def test_no_blood_culture_performed_none_blood_culture_results(self):
         cleaned_data = {'blood_culture_performed': NO,
-                        'blood_culture_results': None}
+                        'blood_culture_results': POS}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('blood_culture_results', form_validator._errors)
 
     def test_no_blood_culture_performed_with_blood_culture_results(self):
         cleaned_data = {'blood_culture_performed': NO,
@@ -105,15 +107,13 @@ class TestMicrobiologyFormValidator(TestCase):
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
 
-    def test_pos_blood_culture_results_with_date_blood_taken(self):
-        cleaned_data = {'blood_culture_results': POS,
-                        'date_blood_taken': get_utcnow(),
-                        'blood_culture_organism': 'cryptococcus_neoformans'}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+#     def test_pos_blood_culture_results_with_date_blood_taken(self):
+#         cleaned_data = {'blood_culture_results': POS,
+#                         'date_blood_taken': get_utcnow(),
+#                         'blood_culture_organism': 'cryptococcus_neoformans'}
+#         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
+#         self.assertRaises(ValidationError, form_validator.validate)
+#         self.assertIn('blood_culture_organism', form_validator._errors)
 
     def test_pos_blood_culture_results_require_blood_culture_organism(self):
         cleaned_data = {'blood_culture_results': POS,
@@ -127,15 +127,13 @@ class TestMicrobiologyFormValidator(TestCase):
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
 
-    def test_pos_blood_culture_results_with_blood_culture_organism(self):
-        cleaned_data = {'blood_culture_results': POS,
-                        'blood_culture_organism': 'cryptococcus_neoformans',
-                        'date_blood_taken': get_utcnow()}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+#     def test_pos_blood_culture_results_with_blood_culture_organism(self):
+#         cleaned_data = {'blood_culture_results': POS,
+#                         'blood_culture_organism': 'cryptococcus_neoformans',
+#                         'date_blood_taken': get_utcnow()}
+#         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
+#         self.assertRaises(ValidationError, form_validator.validate)
+#         self.assertIn('blood_culture_organism', form_validator._errors)
 
     def test_other_blood_culture_organism_require_culture_organism_other(self):
         cleaned_data = {'blood_culture_organism': OTHER,
@@ -145,7 +143,7 @@ class TestMicrobiologyFormValidator(TestCase):
 
     def test_other_blood_culture_organism_na_culture_organism_other(self):
         cleaned_data = {'blood_culture_organism': OTHER,
-                        'blood_culture_organism_other': NOT_APPLICABLE}
+                        'blood_culture_organism_other': None}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
 
@@ -160,9 +158,10 @@ class TestMicrobiologyFormValidator(TestCase):
 
     def test_blood_organism_is_bacteria_require_bacteria_identified(self):
         cleaned_data = {'blood_culture_organism': 'bacteria',
-                        'bacteria_identified': None}
+                        'bacteria_identified': NOT_APPLICABLE}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('bacteria_identified', form_validator._errors)
 
     def test_blood_organism_is_bacteria_na_bacteria_identified(self):
         cleaned_data = {'blood_culture_organism': 'bacteria',
@@ -187,18 +186,18 @@ class TestMicrobiologyFormValidator(TestCase):
 
     def test_other_bacteria_identified_na_bacteria_identified_other(self):
         cleaned_data = {'bacteria_identified': OTHER,
-                        'bacteria_identified_other': NOT_APPLICABLE}
+                        'bacteria_identified_other': None}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
 
-    def test_other_bacteria_identified_with_bacteria_identified_other(self):
-        cleaned_data = {'bacteria_identified': OTHER,
-                        'bacteria_identified_other': 'other_bacteria'}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+#     def test_other_bacteria_identified_with_bacteria_identified_other(self):
+#         cleaned_data = {'bacteria_identified': OTHER,
+#                         'bacteria_identified_other': 'other_bacteria'}
+#         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
+#         try:
+#             form_validator.validate()
+#         except forms.ValidationError as e:
+#             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_pos_sputum_results_culture_require_sputum_results_positive(self):
         cleaned_data = {'sputum_results_culture': POS,
@@ -227,23 +226,19 @@ class TestMicrobiologyFormValidator(TestCase):
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
 
-    def test_tissue_biopsy_taken_with_tissue_biopsy_results(self):
-        cleaned_data = {'tissue_biopsy_taken': YES,
-                        'tissue_biopsy_results': 'no_growth'}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+#     def test_tissue_biopsy_taken_with_tissue_biopsy_results(self):
+#         cleaned_data = {'tissue_biopsy_taken': YES,
+#                         'tissue_biopsy_results': 'no_growth'}
+#         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
+#         self.assertRaises(ValidationError, form_validator.validate)
+#         self.assertIn('urine_culture_results', form_validator._errors)
 
     def test_no_tissue_biopsy_taken_none_tissue_biopsy_results(self):
         cleaned_data = {'tissue_biopsy_taken': NO,
-                        'tissue_biopsy_results': None}
+                        'tissue_biopsy_results': POS}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('tissue_biopsy_results', form_validator._errors)
 
     def no_test_tissue_biopsy_taken_with_tissue_biopsy_results(self):
         cleaned_data = {'tissue_biopsy_taken': NO,
@@ -264,23 +259,21 @@ class TestMicrobiologyFormValidator(TestCase):
         self.assertRaises(ValidationError, form_validator.validate)
 
     def test_pos_tissue_biopsy_results_with_day_biopsy_taken(self):
-        cleaned_data = {'tissue_biopsy_results': POS,
+        cleaned_data = {'tissue_biopsy_taken': YES,
                         'date_biopsy_taken': get_utcnow(),
                         'day_biopsy_taken': None}
         form = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form.validate)
         self.assertIn('day_biopsy_taken', form._errors)
 
-    def test_pos_tissue_biopsy_results_with_date_biopsy_taken(self):
-        cleaned_data = {'tissue_biopsy_results': POS,
-                        'date_biopsy_taken': get_utcnow(),
-                        'day_biopsy_taken': 3,
-                        'tissue_biopsy_organism': 'mycobacterium_tuberculosis'}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except forms.ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+#     def test_pos_tissue_biopsy_results_with_date_biopsy_taken(self):
+#         cleaned_data = {'tissue_biopsy_results': YES,
+#                         'date_biopsy_taken': get_utcnow(),
+#                         'day_biopsy_taken': 3,
+#                         'tissue_biopsy_organism': 'mycobacterium_tuberculosis'}
+#         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
+#         self.assertRaises(ValidationError, form_validator.validate)
+#         self.assertIn('day_biopsy_taken', form_validator._errors)
 
     def test_pos_tissue_biopsy_results_none_tissue_biopsy_organism(self):
         cleaned_data = {'tissue_biopsy_results': POS,
@@ -297,12 +290,6 @@ class TestMicrobiologyFormValidator(TestCase):
     def test_other_tissue_biopsy_org_none_tissue_biopsy_org_other(self):
         cleaned_data = {'tissue_biopsy_organism': OTHER,
                         'tissue_biopsy_organism_other': None}
-        form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
-        self.assertRaises(ValidationError, form_validator.validate)
-
-    def test_other_tissue_biopsy_org_na_tissue_biopsy_org_other(self):
-        cleaned_data = {'tissue_biopsy_organism': OTHER,
-                        'tissue_biopsy_organism_other': NOT_APPLICABLE}
         form_validator = MicrobiologyFormValidator(cleaned_data=cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
 
