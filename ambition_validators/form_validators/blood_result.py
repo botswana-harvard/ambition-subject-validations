d69798af5c7ae9d2c9843f269938fad7d9c3009a
aseokana <aseokana@bhp.org.bw>
2017-11-23 07:24:18 +0200

    removing in

--- ambition_validators/form_validators/blood_result.py
+++ ambition_validators/form_validators/blood_result.py
@@ -62,10 +62,10 @@ class BloodResultFormValidator(FormValidator):
 
     def creatinine(self, field=None, cleaned_data=None):
         if (self.cleaned_data.get('creatinine_unit')
-            and ((self.cleaned_data.get('creatinine_unit') in 'mg/dL'
+            and ((self.cleaned_data.get('creatinine_unit') == 'mg/dL'
                   and (self.cleaned_data.get(field) < 0.6
                        or self.cleaned_data.get(field) > 1.3)) or
-                 (self.cleaned_data.get('creatinine_unit') in
+                 (self.cleaned_data.get('creatinine_unit') ==
                   'umol/L' and (self.cleaned_data.get(field) < 53
                                 or self.cleaned_data.get(field) > 115)))):
             if self.cleaned_data.get('are_results_normal') != NO:
